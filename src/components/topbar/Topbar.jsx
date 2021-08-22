4f701f0c04f6fe928324c2008fe0d3983eb7655b
MTayyebKhan <tayyeb.10@live.com>
2021-08-22 19:43:42 +1000

    styling of the top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,15 @@ import "./topbar.scss"
 function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">LEFT</a>
+                </div>
+                <h1>center</h1>
+                <div className="right">
+                    RIGHT
+                </div>
+            </div>
         </div>
     )
 }
