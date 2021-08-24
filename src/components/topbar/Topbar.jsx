49ea8ed4aeaeef9a600741df8b88c466ad3b5c91
MTayyebKhan <tayyeb.10@live.com>
2021-08-24 13:15:52 +1000

    hamburger menu and logo with phone and email added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,26 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">LEFT</a>
+                    <a href="#intro" className="logo">LOGO</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>0415507514</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>tayyeb.10@live.com</span>
+                    </div>
                 </div>
-                <h1>center</h1>
                 <div className="right">
-                    RIGHT
+                    <div className="hamburger">
+                        <MenuIcon />
+                    </div>
                 </div>
             </div>
         </div>
