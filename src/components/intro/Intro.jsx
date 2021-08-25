9903be084b84684d13fd18942ca1a4c4fc849cb7
MTayyebKhan <tayyeb.10@live.com>
2021-08-25 21:43:57 +1000

    intro page with ityped implented

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import "./intro.scss"
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 function Intro() {
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Designer", "Freelancer"],
+        });
+    }, []);
+
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hey, my names</h2>
+                    <h1>Tayyeb Khan</h1>
+                    <h3>Junior <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
