049d22f5868227bff6cfd0da4715a1c7b6b8ee35
MTayyebKhan <tayyeb.10@live.com>
2021-08-28 19:16:23 +1000

    React dynamic portfolio useEffect implentation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio } from "../../data";
 
 function Portfolio() {
     const [selected, setSelected] = useState("featured");
-
+    const [data, setData] = useState([]);
     const list = [
         {
           id: "featured",
@@ -28,6 +29,28 @@ function Portfolio() {
         },
       ];
 
+      useEffect(() => {
+        switch (selected) {
+          case "featured":
+            setData(featuredPortfolio);
+            break;
+          case "web":
+            setData(webPortfolio);
+            break;
+          case "mobile":
+            setData(mobilePortfolio);
+            break;
+          case "design":
+            setData(designPortfolio);
+            break;
+          case "content":
+            setData(contentPortfolio);
+            break;
+          default:
+            setData(featuredPortfolio);
+        }
+      }, [selected]);
+
     return (
         <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
@@ -37,33 +60,15 @@ function Portfolio() {
                 ))}
             </ul>
             <div className="container">
+                {data.map((d) => (
                 <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/globe.png" alt="" />
-                    <h3>Banking App</h3>
-                </div>
-            </div>
-        </div>
-    )
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio
